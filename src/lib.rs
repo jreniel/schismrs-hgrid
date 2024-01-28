0905035c40589a3f6931f01ef37097b09018e9c0
Jaime R Calzada <jreniel@gmail.com>
2024-01-28 10:39:58 -0500

    removed unused files

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 pub use hgrid::Hgrid;
 pub use hgrid::HgridBuilder;
-pub use vgrid::Vgrid;
 
 pub mod boundaries;
 pub mod elements;
 pub mod gr3;
 pub mod hgrid;
 pub mod nodes;
-pub mod traits;
-pub mod vgrid;
